daf8dce8d1f1710bca0be290e680aaacc5eba6cd
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-05 14:14:31 +0300

    Added files via upload

--- create_project_version.rb
+++ create_project_version.rb
@@ -3,11 +3,11 @@ module CreateProjectVersion
     @driver.find_element(:id, 'tab-versions').click
     @wait.until {@driver.find_element(:link, 'New version').displayed?}
     @driver.find_element(:link, 'New version').click
-    @wait.until {@driver.find_element(:id, 'version_name')).displayed?}
+    @wait.until {@driver.find_element(:id, 'version_name').displayed?}
     @new_version_name = ('newname' + rand(99999).to_s)
     @driver.find_element(:id, 'version_name').send_keys @new_version_name
     @driver.find_element(:name, 'commit').click
-    @wait.until {@driver.find_element(:link, @new_version_name)).displayed?}
+    @wait.until {@driver.find_element(:link, @new_version_name).displayed?}
   end
 
 end
