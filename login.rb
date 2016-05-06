493719e1b6103d7a8077c7a4a75e5596d6f3a49c
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-06 16:08:09 +0300

    Delete login.rb

--- login.rb
+++
@@ -1,9 +0,0 @@
-module Login
-  def login
-    @driver.find_element(:class, 'login').click
-    @wait.until {@driver.find_element(:id, 'username').displayed? }
-    @driver.find_element(:id, 'username').send_keys @login
-    @driver.find_element(:id, 'password').send_keys 'Password1'
-    @driver.find_element(:name, 'login').click
-  end
-end
