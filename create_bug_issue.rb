592d3820084ba1ea7048896f0731154f566c4278
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-06 16:09:02 +0300

    Delete create_bug_issue.rb

--- create_bug_issue.rb
+++
@@ -1,9 +0,0 @@
-module CreateBugIssue
-  def create_bug_issue
-    @driver.find_element(:class, 'new-issue').click
-    @wait.until {@driver.find_element(:id, 'issue_subject').displayed? }
-    @driver.find_element(:id, 'issue_subject').send_keys 'This is new Bug issue'
-    @driver.find_element(:name, 'commit').click
-    @wait.until {@driver.find_element(:link, 'View all issues').displayed? }
-  end
-end
