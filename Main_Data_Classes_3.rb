7471cab72a4ca88a9beee6ede805827fa621095d
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-24 10:21:58 +0300

    commit #2 for Task 3 (Main Data Classes)
    the method is updated.

--- Main_Data_Classes_3.rb
+++ Main_Data_Classes_3.rb
@@ -1,5 +1,5 @@
 # 3. There is a string. Find a maximal number (not a digit) in it
 
 def find_max_number (string)
-  string.split(' ').map(&:to_i).max
+  string.gsub!(/\D/, " ").split(' ').map(&:to_i).max
 end
