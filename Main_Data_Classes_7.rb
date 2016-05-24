d284c3484b9e637fb81661dd09548678c6145df5
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-24 12:55:22 +0300

    commit #2 for Task 7 (Main Data Classes)
    updated

--- Main_Data_Classes_7.rb
+++ Main_Data_Classes_7.rb
@@ -2,5 +2,7 @@
 # Modify it with adding ary[0] (first element of the array) to each even number.
 # Don't do it for first and last elements
 
-#ary = [1,2,3,4,5,6,7,8,9]
-ary.slice(1...-1).map{|x| x + ary[0] if x.even?}.compact!
+def method_7(ary)
+ary[1...-1].map{|x| x + ary[0] if x.even?}.compact!
+end
+
