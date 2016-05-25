85451292c20db7f4db28a17b1c147c1ccb862022
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-25 11:14:14 +0300

    Commit #2 for Task 6 (Main Data Classes)
    Updated, please review

--- Main_Data_Classes_6.rb
+++ Main_Data_Classes_6.rb
@@ -1,7 +1,11 @@
 # 6. There is an array of integers (ary). Puts the index of the last element where ary[0]<ary[i]<ary[-1]
 
-last = ary.rindex(ary[0...-1].last)
-puts last
+def method_6 (ary)
+last = ary.select {|i| i > ary[0] && i < ary[-1]}.last
+ary.rindex(last)
+end
+
+
 
 
 
