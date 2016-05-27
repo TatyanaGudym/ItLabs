bfab60a76b241fcdcb9ab4ece26b45028d8ec3ec
Tatyana <tatyana.gudym@rebelmouse.com>
2016-05-27 20:00:06 +0300

    Commit #2 for Task 4 (Main Data Classes)
    Updated. Please take a look

--- Main_Data_classes_4.rb
+++ Main_Data_classes_4.rb
@@ -1,6 +1,19 @@
 # 4. There are two strings. Find number of first matching chars
 
-def method_4 (str1, str2)
-  (str1.chars & str2.chars).length
+def find_number_of_first_matching_chars (str1, str2)
+  counter = 0
+
+  arr1 = str1.chars
+  arr2 = str2.chars
+
+  arr1.map.with_index do |item, index|
+    if item != arr2[index]
+      break
+    end
+
+    counter = counter + 1
+  end
+
+  return counter
 end
 
