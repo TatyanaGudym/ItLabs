aa3ec7ae3f826e0c251a503fab7627cf8ba8ea86
Tatyana <tatyana.gudym@rebelmouse.com>
2016-07-12 17:33:02 +0300

    Scenario file is updated
    Now it has unit tests.

--- test_trello_scenario.rb
+++ test_trello_scenario.rb
@@ -5,6 +5,7 @@
 #comments
 #Create 10 any unit tests for this classes and their methods
 
+require 'test/unit'
 require_relative "lists"
 require_relative "comments"
 require_relative "cards"
@@ -13,126 +14,152 @@ require_relative "team"
 require_relative "users"
 
 
+class TestTrello < Test::Unit::TestCase
+
+  def setup
+    @user = Users.new
+    @team = Team.new
+    @list = Lists.new
+    @done_list = Lists.new
+    @board = Boards.new
+    @card = Cards.new
+    @new_comment = Comments.new
+    @new_card = @list.create_new_card_for_list(@list)
+  end
 
 #create new user
-user = Users.new
-puts "========================"
-puts "USER"
-puts "========================"
-puts user
+def test_create_user
+  @user
+  puts "========================"
+  puts "USER"
+  puts "========================"
+  puts @user
+end
 
 #create new team
-team = Team.new
+def test_create_team
+@team
 puts "========================"
 puts "THE TEAM"
 puts "========================"
-puts team
+puts @team
+end
 
 #add user to the team
-team.members << user
+  def test_add_user_to_team
+@team.members << @user
 puts "========================"
 puts "TEAM MEMBERS"
 puts "========================"
-puts team.members
+puts @team.members
+end
 
 
 # add new user with a name and puts the list of all users
-team.add_new_user('Tanya')
+def test_named_user
+  @team.add_new_user('Tanya')
 puts "========================"
 puts "ALL USERS IN THE TEAM"
 puts "========================"
-puts team.members
+puts @team.members
+end
 
 
 #create new board
-board = Boards.new
+def test_new_board
+@board
 puts "========================"
 puts "BOARDS"
 puts "========================"
-puts board
+puts @board
+end
 
 #create two new lists
-list = Lists.new
-done_list = Lists.new
-
+  def test_create_two_lists
+@list
+@done_list
 puts "========================"
 puts "INITIAL LIST"
 puts "========================"
-puts list
+puts @list
 puts "========================"
 puts "DONE LIST"
 puts "========================"
-puts done_list
+puts @done_list
+end
 
 
 #create new card
-card = Cards.new
-list.cards << card
+  def test_create_new_card
+@card
+@list.cards << @card
 puts "========================"
 puts "CARDS"
 puts "========================"
-puts list.cards
+puts @list.cards
+end
 
 #create new card for the first list
-new_card = list.create_new_card_for_list(list)
-list.cards << new_card
+  def test_create_new_card_for_list
+    @new_card
+@list.cards << @new_card
 puts "========================"
 puts "CARDs with NEW"
 puts "========================"
-puts list.cards
+puts @list.cards
+end
 
 #show crads in initial list
+  def test_show_cards_in_initial_list
 puts "========================"
 puts "CARDS in INITIAL LIST"
 puts "========================"
-list.show_cards_in_list(list)
+@new_card
+@list.cards << @new_card
+@list.show_cards_in_list(@list)
+end
 
 
 #move card to other done_list
-new_card.move_card_to_another_list(done_list)
+def test_move_card_to_done_list
+  @new_card.move_card_to_another_list(@done_list)
 puts "========================"
 puts "CARD was moved from initial to done"
 puts "========================"
-
-#show cards in done_list
-puts "========================"
-puts "CARDS in DONE"
-puts "========================"
-list.show_cards_in_list(done_list)
-
-#show crads in initial list
-puts "========================"
-puts "CARDS in INITIAL LIST"
-puts "========================"
-list.show_cards_in_list(list)
-
+end
 
 
 #add checklist to the card
-new_checklist = card.add_checklist("Hello checklist!")
+  def test_add_checklist_to_card
+new_checklist = @card.add_checklist("Hello checklist!")
 puts "========================"
 puts "Checklists"
 puts "========================"
-puts card.checklist
+puts @card.checklist
+end
 
 
 #create new comment
-new_comment = Comments.new
-new_comment = card.add_new_comment("Hello this is comment")
+  def test_create_new_comment
+@new_comment = @card.add_new_comment("Hello this is comment")
 puts "========================"
 puts "Comments"
 puts "========================"
-puts card.comments
+puts @card.comments
+end
 
 
 #delete comment
-card.delete_comment("Hello this is comment")
+  def test_delete_comment
+    @new_comment = @card.add_new_comment("Hello this is comment for delete")
+@card.delete_comment("Hello this is comment for delete")
 puts "========================"
 puts "Comments after delete"
 puts "========================"
-puts card.comments
+puts @card.comments
+  end
 
-
+  end
 
 
 
